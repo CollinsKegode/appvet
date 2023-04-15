cb2b8bbff0c743b80a7108492c1c40d2b063e8d1
CollinsKegode <collinskegode@gmail.com>
2023-04-15 14:05:09 +0300

    Add the app routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mysql from "mysql";
 import session from "express-session";
-import bycrypt from "bycrypt";
+import bcrypt from "bcrypt";
 
 // create expressjs app
 const app = express()
@@ -28,6 +28,24 @@ app.use(express.urlencoded({extended: false}))
 
 // serving public file
 app.use(express.static('public'))
+
+// Route to homepage
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// Route to login page
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+// Route to signup page
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+
+
 
 app.listen(5000, () => {
     console.log('app is running...');
